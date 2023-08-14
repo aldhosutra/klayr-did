56cfba0ab901beb12448d663c9e0c379a1d50840
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-08-14 11:39:43 +0800

    setup eslint for jest testing

---
+++ packages/lisk-did-module/test/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['lisk-base/ts-jest'],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/no-unsafe-assignment': ['off'],
+    '@typescript-eslint/no-unsafe-member-access': ['off'],
+    '@typescript-eslint/no-unsafe-call': ['off'],
+    '@typescript-eslint/no-unsafe-return': ['off'],
+    '@typescript-eslint/no-extraneous-class': ['off'],
+    '@typescript-eslint/member-ordering': ['off'],
+    'no-console': ['off'],
+    '@typescript-eslint/explicit-member-accessibility': ['off'],
+    '@typescript-eslint/no-namespace': ['off'],
+    '@typescript-eslint/no-non-null-assertion': ['off'],
+    'prefer-promise-reject-errors': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'no-useless-constructor': ['off'],
+    'import/no-unresolved': ['off'],
+    'import/extensions': ['off'],
+    '@typescript-eslint/prefer-nullish-coalescing': ['off'],
+  },
+};
