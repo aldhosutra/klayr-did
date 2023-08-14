89aad1124015a382facca5e26db29e458369cc49
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-08-14 10:06:35 +0800

    change wild import to default import in lisk-did package

--- packages/lisk-did/src/index.ts
+++ packages/lisk-did/src/index.ts
@@ -1,6 +1,6 @@
-import * as didModule from '@lisk-did/lisk-did-module';
-import * as did from '@lisk-did/lisk-decentralized-identifier';
-import * as vc from '@lisk-did/lisk-verifiable-credentials';
+import didModule from '@lisk-did/lisk-did-module';
+import did from '@lisk-did/lisk-decentralized-identifier';
+import vc from '@lisk-did/lisk-verifiable-credentials';
 
 export * from '@lisk-did/lisk-did-module';
 export * as did from '@lisk-did/lisk-decentralized-identifier';
